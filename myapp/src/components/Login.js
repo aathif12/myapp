ffe236f1991e591f7a0944bba011322c213c0323
aathif12 <aathifahamad4@gmail.com>
2024-11-26 23:07:46 +0530

    Styles For Login

--- myapp/src/components/Login.js
+++ myapp/src/components/Login.js
@@ -1,6 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
-
+import "./Login.css";
 export default function Login() {
   const navigate = useNavigate();
   const [username, setUsername] = useState("");
