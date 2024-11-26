4160d9571c6385fac4f318aa8570be1c6cf0bdfe
aathif12 <aathifahamad4@gmail.com>
2024-11-26 14:17:01 +0530

    Routing Final

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -2,14 +2,35 @@ import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home';
 import {BrowserRouter as Router,Routes,Route,Link,NavLink} from 'react-router-dom';
+import Contact from './components/Contact';
+import Aboutus from './components/Aboutus';
 
 function App() {
   return (
     <div className="App">
+
       <Router>
-        <Route path='/Home' element={<Home/>}></Route>
+
+        <nav>
+          <ul>
+            <li>
+              <Link to='/home'>Home</Link>
+            </li>
+            <li>
+              <Link to='/contact'>Contact</Link>
+            </li>
+            <li>
+              <Link to='/aboutus'>Abous</Link>
+            </li>
+          </ul>
+        </nav>
+     
+      <Routes>
+        <Route path='/home' element={<Home/>}></Route>
+        <Route path='/contact' element={<Contact/>}></Route>
+        <Route path='/aboutus' element={<Aboutus/>}></Route>
+      </Routes>
       </Router>
-    
     </div>
   );
 }
--- myapp/src/components/Aboutus.js
+++ myapp/src/components/Aboutus.js
@@ -1,4 +1,4 @@
-export default Aboutus() = ()=>{
+export default function Aboutus(){
     return(
         <div>
 
